74b1fb934878f87be254c650457027a68a64b90d
Julio Pereira <juliocp.next@gmail.com>
2022-06-29 10:42:25 -0300

    Update EditorStringsProvider.cs

--- Editor/Scripts/EditorStringsProvider.cs
+++ Editor/Scripts/EditorStringsProvider.cs
@@ -1,16 +1,42 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GMBEditor
 {
     public class EditorStringsProvider
     {
+        private static string _ROOTFOLDER_
+        {
+            get
+            {
+                Assembly currentAssembly = typeof(GMB.Data).Assembly;
+                List<AssemblyName> referencedAssemblies = currentAssembly.GetReferencedAssemblies().ToList();
+
+                if (referencedAssemblies.Exists(r => r == currentAssembly.GetName()))
+                {
+                    return _PACKAGEPATH_;
+                }
+
+
+                return _PATH_GMB_;
+
+
+            }
+        }
+
+        public const string _PACKAGEPATH_ = "Packages/com.neoneper.gmb/";
         public const string _PATH_GMB_ = "Assets/GMB-Master/";
-        public const string _PATH_GMB_RESOURCES_ = _PATH_GMB_ + "Behaviour/Resources/";
-        public const string _PATH_GMB_EDITOR_ = _PATH_GMB_ + "Editor/";
-        public const string _PATH_GMB_EDITOR_RESOURCES_ = _PATH_GMB_EDITOR_ + "Resources/";
-        public const string _PATH_GMB_EDITOR_TEMPLATES_ = _PATH_GMB_EDITOR_RESOURCES_ + "VisualTemplates/";
-        public const string _PATH_GMB_EDITOR_TEMPLATES_DEFAULTS = _PATH_GMB_EDITOR_TEMPLATES_ + "Defaults/";
+
+
+
+        public static string _PATH_GMB_RESOURCES_ = _ROOTFOLDER_ + "Behaviour/Resources/";
+        public static string _PATH_GMB_EDITOR_ = _ROOTFOLDER_ + "Editor/";
+        public static string _PATH_GMB_EDITOR_RESOURCES_ = _PATH_GMB_EDITOR_ + "Resources/";
+        public static string _PATH_GMB_EDITOR_TEMPLATES_ = _PATH_GMB_EDITOR_RESOURCES_ + "VisualTemplates/";
+        public static string _PATH_GMB_EDITOR_TEMPLATES_DEFAULTS = _PATH_GMB_EDITOR_TEMPLATES_ + "Defaults/";
 
 
     }
